790892ffd1561bc62cc6f1a16d2bdcc36d70d972
Rafael Ocariz <rafael.t1336.beyond@xpi.com.br>
2019-10-02 09:58:48 -0300

    #Added a Last-in First-out array (AKA Stack) #Improved the DoTryCatch function. #Added a custom message for unhandled exceptions that cause software termination.

---
+++ PluralSight_Course/LifoArray.h
@@ -0,0 +1,122 @@
+#pragma once
+
+#include <cassert>
+#include <iostream>
+
+template<typename T>
+class LifoArray
+{
+	int m_size;
+	int m_top;
+	T* m_array = nullptr;
+
+public:
+	explicit LifoArray(int size) : m_size(size), m_top(-1)
+	{
+		assert(m_size > 0, "ERROR: Array size must be greater than 0.");
+		m_array = new T[m_size];
+	}
+
+	LifoArray(const LifoArray& source) : m_size(source.m_size), m_top(source.m_top)
+	{
+		m_array = new T[m_size];
+
+		for (int i = 0; i <= m_top; i++)
+		{
+			m_array[i] = source.m_array[i];
+		}
+	}
+
+	LifoArray(LifoArray&& source) : m_size(source.m_size), m_top(source.m_top), m_array(source.m_array)
+	{
+		source.m_array = nullptr;
+		source.m_size = 0;
+		source.m_top = -1;
+	}
+
+	LifoArray& operator = (const LifoArray& source)
+	{
+		m_size = source.m_size;
+		m_top = source.m_top;
+
+		if (m_array) delete[]m_array;
+		m_array = new T[m_size];
+
+		for (int i = 0; i <= m_top; i++)
+		{
+			m_array[i] = source.m_array[i];
+		}
+		return *this;
+	}
+
+	LifoArray& operator = (LifoArray&& source)
+	{
+		m_size = source.m_size;
+		m_top = source.m_top;
+
+		if (m_array) delete[]m_array;
+
+		if (!source.m_array) m_array = new T[m_size];
+		else m_array = source.m_array;
+
+		source.m_size = 0;
+		source.m_top = -1;
+		source.m_array = nullptr;
+		return *this;
+	}
+
+	~LifoArray()
+	{
+		if (m_array)
+			delete []m_array;
+	}
+
+	void Push(const T& item) noexcept
+	{
+		if (m_top == (m_size - 1))
+			throw std::range_error("Stack is already Full.");
+
+		++m_top;
+		m_array[m_top] = item;
+	}
+
+	T Pop() noexcept
+	{
+		const auto &ret = Top();
+
+		m_array[m_top] = 0;
+		--m_top;
+
+		return ret;
+	}
+
+	T Top() const noexcept
+	{
+		if (m_top == -1)
+			throw std::exception("Stack is Empty.");
+
+		return m_array[m_top];
+	}
+
+	int Size() const noexcept { return m_size; }
+
+	int CurrentStackSize() const noexcept { return (m_top + 1); }
+
+	friend std::ostream& operator << (std::ostream& out, const LifoArray<T>& source)
+	{
+		int top = source.m_top;
+
+		out << "[Top] " << source.m_array[top];
+		--top;
+		while (top >= 0)
+		{
+			out << " -> " << source.m_array[top];
+			--top;
+		}
+
+		out << " [Bottom]";
+
+		return out;
+	}
+};
+
--- PluralSight_Course/Main.cpp
+++ PluralSight_Course/Main.cpp
@@ -2,6 +2,12 @@
 #include <chrono>
 #include "Array.h"
 #include "SortedArray.h"
+#include "LifoArray.h"
+
+void UnexpectedError()
+{
+	std::cout << "An Unexpected error happened, contact SAC team." << std::endl;
+}
 
 void DoTryCatch(std::function<void()> func)
 {
@@ -9,14 +15,24 @@ void DoTryCatch(std::function<void()> func)
 	{
 		func();
 	}
-	catch (std::exception &ex)
+	catch (std::range_error& ex)
+	{
+		std::cout << "RANGE_ERROR: " << ex.what() << std::endl;
+	}
+	catch (std::exception& ex)
 	{
 		std::cout << "ERROR: " << ex.what() << std::endl;
+	}
+	catch (...)
+	{
+		UnexpectedError();
 	}
 }
 
 int main()
 {
+	std::set_terminate(UnexpectedError);
+
 	int size = 10000000;
 	Array<int> lista(size);
 
@@ -83,15 +99,51 @@ int main()
 		sLista.Insert(8);
 		sLista.Insert(9);*/
 
-		std::cout << "Insert ordered Array: " << std::chrono::duration_cast<std::chrono::nanoseconds>(endTime - beginTime).count() << " nanoseconds" << std::endl;
+		/*std::cout << "Insert ordered Array: " << std::chrono::duration_cast<std::chrono::nanoseconds>(endTime - beginTime).count() << " nanoseconds" << std::endl;
 
-		// std::cout << sLista << std::endl;
+		std::cout << sLista << std::endl;
 		std::cout << sLista[5] << std::endl;
 		beginTime = std::chrono::time_point< std::chrono::system_clock>();
 		std::cout << sLista.Find(4590) << std::endl;
 		endTime = std::chrono::time_point< std::chrono::system_clock>();
 
 		std::cout << "Find ordered Array: " << std::chrono::duration_cast<std::chrono::nanoseconds>(endTime - beginTime).count() << " nanoseconds" << std::endl;
+	*/});
+
+	DoTryCatch([]()
+	{
+		LifoArray<int> lifo_first(10);
+
+		std::cout << "Total size of the Stack = " << lifo_first.Size() << std::endl;
+		std::cout << "Current size of the Stack = " << lifo_first.CurrentStackSize() << std::endl;
+
+		lifo_first.Push(10);
+		lifo_first.Push(8);
+		lifo_first.Push(9);
+		lifo_first.Push(5);
+		lifo_first.Push(6);
+		lifo_first.Push(4);
+		lifo_first.Push(7);
+		lifo_first.Push(3);
+		lifo_first.Push(1);
+		lifo_first.Push(2);
+
+		std::cout << "First stack: " << lifo_first << std::endl;
+
+		auto lifo_second = lifo_first;
+
+		std::cout << "Second stack: " << lifo_second << std::endl;
+
+		auto lifo_copy = std::move(lifo_first);
+
+		std::cout << "Moved stack: " << lifo_copy << std::endl;
+
+		// It needs to be done in two lines
+		// For some kind of reason it's executing in an unordered way on vs2019
+		std::cout << lifo_copy.Top() << " | ";
+		std::cout << lifo_copy.CurrentStackSize() << " | " << lifo_copy << std::endl;
+		std::cout << lifo_copy.Pop() << " | ";
+		std::cout << lifo_copy.CurrentStackSize() << " | " << lifo_copy << std::endl;
 	});
 
 	return 0;
